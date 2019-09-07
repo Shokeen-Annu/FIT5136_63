9351573575f487058c7fd218c964b612657c7dc3
zlii0122 <zlii0122@student.monash.edu>
2019-09-07 18:28:52 +1000

    some basic function in the user

--- src/User.java
+++ src/User.java
@@ -5,5 +5,66 @@ public class User {
     private String emailAdd;
     private String passWord;
 
+    public User()
+    {
+        firstName = " ";
+        lastName = " ";
+        phoneNo = 0 ;
+        emailAdd = " ";
+        passWord = " ";
+    }
 
+    public User (String newFirstName, String newLastName, int newPhoneNo, String newEmailAdd, String newPassWord)
+    {
+        firstName = newFirstName;
+        lastName = newLastName;
+        phoneNo = newPhoneNo;
+        emailAdd = newEmailAdd;
+        passWord = newPassWord;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String displayUser()
+    {
+        return firstName + " " + lastName  + "\r\n" + "phone number: " + phoneNo + "\r\n" + "email address: "+ emailAdd;
+    }
 }
