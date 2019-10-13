b7893cc28fb4c0f96e7b707f275281914f596e10
zlii0122 <zlii0122@student.monash.edu>
2019-10-13 13:49:44 +1100

    doing discount

---
+++ src/OwnerController.java
@@ -0,0 +1,8 @@
+public class OwnerController {
+    public boolean addDiscount()
+    {
+
+
+    }
+}
+
--- src/OwnerInterface.java
+++ src/OwnerInterface.java
@@ -7,7 +7,16 @@ public class OwnerInterface {
        System.out.println("2 EDIT DISCOUNT");
        System.out.println("3 DELETE DISCOUNT");
        choiceNumber = PrimeEvents.receiveInt();
-
-
+       switch(choiceNumber)
+       {
+           case 1:
+               break;
+           case 2:
+               break;
+           case 3:
+               break;
+           default: System.out.println("Please enter the option correctly");
+           break;
+       }
    }
 }
