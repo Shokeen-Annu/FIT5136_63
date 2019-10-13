817e5f10c76fa4f8f04895305d5c62dee1f7b4fa
zlii0122 <zlii0122@student.monash.edu>
2019-10-13 18:36:22 +1100

    Mia part

--- src/OwnerController.java
+++ src/OwnerController.java
@@ -1,8 +1,49 @@
 public class OwnerController {
-    public boolean addDiscount()
+    private boolean addHall(Hall)
     {
 
-      return false;
     }
+
+    private Hall generateHallObject()
+    {
+
+    }
+    private boolean deleteHall(Hall)
+    {
+
+    }
+    private boolean updateHall(Hall)
+    {
+
+    }
+    private void viewHalls(String)
+    {
+
+    }
+    private void viewReviewOfHall(int)
+    {
+
+    }
+    private Quotation provideQuotation(int)
+    {
+
+    }
+    private boolean cancelBooking(int)
+    {
+
+    }
+    private void viewBookingHistory(int)
+    {
+
+    }
+    private boolean addDiscount(String, double, int, String)
+    {
+
+    }
+    private boolean isHallPresent(int)
+    {
+
+    }
+
 }
 
---
+++ src/Quotation.java
@@ -0,0 +1,90 @@
+import java.util.Date;
+
+public class Quotation {
+    private int quotationId;
+    private Date date;
+    private Date bookingStartDate;
+    private Date bookingFinishDate;
+    private int numberOfGuest;
+    private int userId;
+    private int hallId;
+    private Decimal price;
+    private boolean isCatering;
+    private String typeOfMeal;
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getHallId() {
+        return hallId;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setHallId(int hallId) {
+        this.hallId = hallId;
+    }
+
+    public int getQuotationId() {
+        return quotationId;
+    }
+
+    public void setQuotationId(int quotationId) {
+        this.quotationId = quotationId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getNumberOfGuest() {
+        return numberOfGuest;
+    }
+
+    public void setNumberOfGuest(int numberOfGuest) {
+        this.numberOfGuest = numberOfGuest;
+    }
+
+    public Decimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(Decimal price) {
+        this.price = price;
+    }
+    public boolean isCatering() {
+        return isCatering;
+    }
+
+    public void setCatering(boolean catering) {
+        isCatering = catering;
+    }
+    public String getTypeOfMeal() {
+        return typeOfMeal;
+    }
+
+    public void setTypeOfMeal(String typeOfMeal) {
+        this.typeOfMeal = typeOfMeal;
+    }
+    public Date getBookingStartDate() {
+        return bookingStartDate;
+    }
+
+    public void setBookingStartDate(Date bookingStartDate) {
+        this.bookingStartDate = bookingStartDate;
+    }
+
+    public Date getBookingFinishDate() {
+        return bookingFinishDate;
+    }
+
+    public void setBookingFinishDate(Date bookingFinishDate) {
+        this.bookingFinishDate = bookingFinishDate;
+    }
+
+}
